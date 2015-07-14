39d8fe097b904fcb4b587d95fff82cf2aeb5578a
Kyle-Muir <kyle.matthew.muir@gmail.com>
2015-07-15 08:04:55 +1200

    Moving from PROD environment to IMPL

--- Source/Test/Tracman.Tenrox.Integration.Test/TenroxConstants.cs
+++ Source/Test/Tracman.Tenrox.Integration.Test/TenroxConstants.cs
@@ -4,10 +4,10 @@ namespace Tracman.Tenrox.Integration.Test
 {
     public static class TenroxConstants
     {
-        public static Uri LogonServiceUri = new Uri("https://intergen.tenrox.net/Twebservice/logonas.svc");
-        public static Uri TimesheetsServiceUri = new Uri("https://intergen.tenrox.net/Twebservice/timesheets.svc");
-        public static Uri AssignmentsServiceUri = new Uri("https://intergen.tenrox.net/Twebservice/sdk/appointments.svc");
-        public static Uri ClientsServiceUri = new Uri("https://intergen.tenrox.net/Twebservice/sdk/clients.svc");
+        public static Uri LogonServiceUri = new Uri("https://intergenimpl.tenrox.net/Twebservice/logonas.svc");
+        public static Uri TimesheetsServiceUri = new Uri("https://intergenimpl.tenrox.net/Twebservice/timesheets.svc");
+        public static Uri AssignmentsServiceUri = new Uri("https://intergenimpl.tenrox.net/Twebservice/sdk/appointments.svc");
+        public static Uri ClientsServiceUri = new Uri("https://intergenimpl.tenrox.net/Twebservice/sdk/clients.svc");
         public const int IntergenBusinessSlns = 1340;
     }
 }
